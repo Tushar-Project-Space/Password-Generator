daeed2459397868af01a65cc4c2726b6887a5118
Tushar More <158036610+Tushar-Project-Space@users.noreply.github.com>
2024-06-06 20:48:41 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,91 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const App = () => {
-
-  const [length, setLength] = useState(8)
-  const [numAllow, setNumAllow] = useState(false)
-  const [charAllow, setCharAllow] = useState(false)
-  const [password, setPassword] = useState("")
-  const passwordRef = useRef(null)
+  const [length, setLength] = useState(8);
+  const [numAllow, setNumAllow] = useState(false);
+  const [charAllow, setCharAllow] = useState(false);
+  const [password, setPassword] = useState("");
+  const passwordRef = useRef(null);
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    if(numAllow) str += '0123456789';
+    if (numAllow) str += "0123456789";
 
-    if(charAllow) str += '!@#$%^&*()_+';
+    if (charAllow) str += "!@#$%^&*()_+";
 
-    for(let i = 0; i < length; i++){
-      let char = (Math.floor(Math.random() * str.length + 1))
-      pass += str.charAt(char)
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
     }
     setPassword(pass);
-  },[length, numAllow, charAllow, setPassword])
+  }, [length, numAllow, charAllow, setPassword]);
 
-  useEffect(() => {passwordGenerator()}, [length, numAllow, charAllow, setPassword])
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numAllow, charAllow, setPassword]);
 
   const copyToClipBoard = useCallback(() => {
-    navigator.clipboard.writeText(password)
-    passwordRef.current?.select()
-    passwordRef.current?. setSelectionRange(0, 20)
-  },[password])
+    navigator.clipboard.writeText(password);
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 20);
+  }, [password]);
 
   return (
     <>
-  <div className="title">Random Password Generator</div>
+      <div className="title">Random Password Generator</div>
 
-    <div className='container'>
-      <div className='inputBtn'>
-        <input type="text" 
-        value={password} 
-        readOnly 
-        ref={passwordRef}
-        placeholder='password'/>
+      <div className="container">
+        <div className="inputBtn">
+          <input
+            type="text"
+            value={password}
+            readOnly
+            ref={passwordRef}
+            placeholder="password"
+          />
 
-        <button onClick={copyToClipBoard}>Copy</button>
+          <button onClick={copyToClipBoard}>Copy</button>
+        </div>
+
+        <div className="range">
+          <input
+            type="range"
+            min={8}
+            max={30}
+            value={length}
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+          />
+
+          <label>Length : {length}</label>
+        </div>
+
+        <div className="check">
+          <input
+            type="checkbox"
+            id="num"
+            defaultChecked={numAllow}
+            onChange={() => {
+              setNumAllow((prev) => !prev);
+            }}
+          />
+          <label htmlFor="num">Number</label>
+
+          <input
+            type="checkbox"
+            id="char"
+            defaultChecked={charAllow}
+            onChange={() => {
+              setCharAllow((prev) => !prev);
+            }}
+          />
+          <label htmlFor="char">Character</label>
+        </div>
       </div>
-
-      <div className='range'>
-        <input type="range"
-        min={8}
-        max={30}
-        value={length} 
-        onChange={(e) => {setLength(e.target.value)}}/>
-
-        <label>Length : {length}</label>
-      </div>
-
-      <div className="check">
-        <input type="checkbox" id='num' 
-        defaultChecked = {numAllow}
-        onChange={() => {setNumAllow((prev) => !prev)}}/>
-        <label htmlFor="num">Number</label>
-
-        <input type="checkbox" id='char'
-        defaultChecked = {charAllow}
-        onChange={() => {setCharAllow((prev) => !prev)}}/>
-        <label htmlFor="char">Character</label>
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
